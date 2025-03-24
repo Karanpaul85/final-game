4b2b509b09b84c1e2ed888c26140150ff333f423
Karanpaul85 <karanpaul85@gmail.com>
2025-03-24 17:01:18 +0530

    update table section

--- src/app/components/tableSection/index.jsx
+++ src/app/components/tableSection/index.jsx
@@ -7,15 +7,19 @@ const TableSection = ({ data, areaData }) => {
           <tr>
             <th>Date</th>
             {areaData.length > 0 &&
-              areaData.map((item) => <th key={item.areaId}>{item.area}</th>)}
+              areaData.map(
+                (item, index) =>
+                  index <= 3 && <th key={item.areaId}>{item.area}</th>
+              )}
           </tr>
         </thead>
         <tbody>
           {data.length > 0 &&
             data.map((item) => (
               <tr key={item.date}>
-                <td>{item.date}</td>
-                {areaData.map((area) => {
+                <td>{item.date.split("-")[0]}</td>
+                {areaData.map((area, index) => {
+                  if (index > 3) return;
                   const result = item.results.find(
                     (res) => res.areaId === area.areaId
                   );
